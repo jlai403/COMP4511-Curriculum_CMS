72594112af84d38003570b9fc32c21f1388ceae2
jlai <j.lai@live.ca>
2014-10-12 14:27:39 -0600

    Updated Navbar

--- View/Dashboard/index.php
+++ View/Dashboard/index.php
@@ -17,35 +17,53 @@ $actionableProgramDtos = FacadeFactory::getDomainFacade()->findProgramsForApprov
 		<link type="text/css" rel="stylesheet" href="/Content/css/bootstrap-3.2.0-dist/bootstrap-theme.min.css" />
 		<link type="text/css" rel="stylesheet" href="/Content/css/theme/layout.css" />
 		<link type="text/css" rel="stylesheet" href="/Content/css/module/colors.css" />
-
 		<link type="text/css" rel="stylesheet" href="/Content/css/theme/dashboard.css" />
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="/Content/css/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+		<script src="/Content/js/errors.js"></script>
 	</head>
 	
 	<body>
-		<div class="nav">
-			<a class="nav-logo" href="/View">
-	      		<img class="logo" src="/Content/img/Mount_Royal_University_Logo.svg.png" />
-	      	</a>
-			
-			<ul>
-				<li>
-					<form action="/Controller/SearchController.php?action=query" method="post">
-						<div class="form-group has-feedback">
-						    <input class="form-control" type="text" name="queryString" placeholder="Search..."/>
-						    <i class="glyphicon glyphicon-search form-control-feedback" style="top:0px;"></i>
-						</div>
-					</form>
-				</li>
-			
-				<li>
-					<a href="/View/Program/Request.php">Create Program</a>
-				</li>
-				<li>
-					<a href="/Controller/UserController.php?action=logout">Logout</a>
-				</li>
-			</ul>
-		</div>
-		
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header" style="height: 150px;">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span> 
+						<span class="icon-bar"></span> 
+						<span class="icon-bar"></span> 
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand nav-logo" href="/View">
+						<img class="logo" src="/Content/img/Mount_Royal_University_Logo.svg.png" />
+					</a>
+				</div>
+	
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<ul class="nav nav-pills navbar-right">
+						<li>
+							<form action="/Controller/SearchController.php?action=query"
+								method="post">
+								<div class="form-group has-feedback">
+									<input class="form-control" type="text" name="queryString"
+										placeholder="Search..." /> <i
+										class="glyphicon glyphicon-search form-control-feedback"
+										style="top: 0px;"></i>
+								</div>
+							</form>
+						</li>
+						
+						<!-- NAV LINKS START -->
+						<li> <a href="/View/Program/Request.php">Create Program</a> </li>
+						<li> <a href="/Controller/UserController.php?action=logout">Logout</a> </li>
+						<!-- NAV LINKS END -->
+					</ul>
+				</div> <!-- /.navbar-collapse -->
+			</div> <!-- /.container-fluid -->
+		</nav>
 		
 		<div class="container">
 			<div class="row">
--- View/Program/Requested.php
+++ View/Program/Requested.php
@@ -14,24 +14,52 @@ $currentUser = SessionManager::authorize();
 		<link type="text/css" rel="stylesheet" href="/Content/css/theme/layout.css" />
 		<link type="text/css" rel="stylesheet" href="/Content/css/theme/create-program.css" />
 		<link type="text/css" rel="stylesheet" href="/Content/css/module/colors.css" />
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="/Content/css/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+		<script src="/Content/js/errors.js"></script>
 	</head>
 
 	<body>
-		<div class="nav">
-			<a class="nav-logo" href="/View">
-	      		<img src="/Content/img/Mount_Royal_University_Logo.svg.png" />
-	      	</a>
-			
-			<ul>
-				<li class="active">
-					<a href="/View/Program/Request.php">Create Program</a>
-				</li>
-				<li>
-					<a href="/Controller/UserController.php?action=logout">Logout</a>
-				</li>
-			</ul>
-		</div>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header" style="height: 150px;">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span> 
+						<span class="icon-bar"></span> 
+						<span class="icon-bar"></span> 
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand nav-logo" href="/View">
+						<img class="logo" src="/Content/img/Mount_Royal_University_Logo.svg.png" />
+					</a>
+				</div>
 	
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<ul class="nav nav-pills navbar-right">
+						<li>
+							<form action="/Controller/SearchController.php?action=query"
+								method="post">
+								<div class="form-group has-feedback">
+									<input class="form-control" type="text" name="queryString"
+										placeholder="Search..." /> <i
+										class="glyphicon glyphicon-search form-control-feedback"
+										style="top: 0px;"></i>
+								</div>
+							</form>
+						</li>
+						
+						<!-- NAV LINKS START -->
+						<li> <a href="/View/Program/Request.php">Create Program</a> </li>
+						<li> <a href="/Controller/UserController.php?action=logout">Logout</a> </li>
+						<!-- NAV LINKS END -->
+					</ul>
+				</div> <!-- /.navbar-collapse -->
+			</div> <!-- /.container-fluid -->
+		</nav>
 	
 		<div class="container form center">
 			<div class="col-md-12 center-text">
